3a6d13d681ec6bcb42827b1e825815e8f3cf2994
Joel Walker <joel@walker9.net>
2017-04-02 18:40:57 -0500

    Switched from antlr submodule to npm module

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-git submodule update --init --recursive
-
 mkdir -p bin/
 curl http://www.antlr.org/download/antlr-4.6-complete.jar -o bin/antlr-4.6-complete.jar
-
-cd antlr4/runtime/JavaScript/src/antlr4/
-    npm install
-cd -
 
